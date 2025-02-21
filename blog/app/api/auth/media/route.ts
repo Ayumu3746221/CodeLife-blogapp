626cc9245dcf5561c3a847d5b502293f1b2e7e8f
Ayumu3746221 <a.kkt0307@gmail.com>
2025-02-21 16:30:51 +0900

    feature-media-upload

--- blog/app/api/auth/media/route.ts
+++ blog/app/api/auth/media/route.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { ConvertMedia } from "@/lib/convert/convertMedia";
 import getMedia from "@/lib/microcms/getMedia";
+import { uploadMedia, UploadMediaResult } from "@/lib/microcms/uploadMedia";
 import { MicroCMSMediaResponse } from "@/type/MicroCMSResponse";
 import { RequiredMediaList } from "@/type/RequiredMedia";
 import { Session } from "next-auth";
@@ -17,6 +18,37 @@ export const GET = async (request: NextRequest): Promise<NextResponse> => {
     return NextResponse.json({ response: data }, { status: 200 });
   } catch (error) {
     console.error("Error | data fetching from microCMS", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
+
+export type UploadMediaAPIResponse = {
+  media: { url: string; status: number };
+};
+
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+  try {
+    const formData = await request.formData();
+    const fileField = formData.get("file");
+    if (!fileField || !(fileField instanceof Blob)) {
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
+    }
+
+    const mediaUrl: UploadMediaResult = await uploadMedia(fileField);
+
+    if (mediaUrl.ok === false) {
+      return NextResponse.json(
+        { error: "Faild upload media" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({ media: { url: mediaUrl.url } }, { status: 200 });
+  } catch (error) {
+    console.error("Error uploading media:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
