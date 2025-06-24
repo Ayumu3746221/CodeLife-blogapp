0e17e0188e8500f88ce9921916e458472d094636
Ayumu3746221 <a.kkt0307@gmail.com>
2025-06-24 20:42:43 +0900

    homepage-performance: Improve drawing speed

--- blog/app/api/article/list/route.ts
+++ blog/app/api/article/list/route.ts
@@ -1,11 +1,16 @@
 import { fetchContentList } from "@/domain/adapter/fetchContentList";
 import { ContentList } from "@/models/contentList/ContentList";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const contentList: ContentList = await fetchContentList();
-    return NextResponse.json({ response: contentList }, { status: 200 });
+    const response = NextResponse.json(
+      { response: contentList },
+      { status: 200 }
+    );
+    response.headers.set("Cache-Control", "s-maxage=60, stale-while-revalidate");
+    return response;
   } catch (error) {
     console.error("Error fetching content list:", error);
     return NextResponse.json(
